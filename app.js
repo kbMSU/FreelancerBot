a57983ef0ed64de964d7f8d62e4e702fe041c779
whanata <whanata1@gmail.com>
2017-02-16 20:29:22 +1100

    Add console log

--- app.js
+++ app.js
@@ -80,12 +80,14 @@ function receivedMessage(event) {
   var contest = new Contest();
 
   if (messageText) {
-
+    console.log(messageText);
    // If we receive a text message, check to see if it matches a keyword
    // and send back the example. Otherwise, just echo the text we received.
     if (contest.sayContest(messageText)) {
+      console.log('contest');
       sendTextMessage(senderID, contest.showContests());
     } else {
+      console.log('normal_message');
       sendTextMessage(senderID, messageText);
     }
   } else if (messageAttachments) {
