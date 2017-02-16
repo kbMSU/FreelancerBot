6e377bf5cfe5293af241bb6db5728ae7c4e62ad6
Karthik <KarBalasubramanian@gmail.com>
2017-02-16 17:06:45 +1100

    Trying to receive first message

--- app.js
+++ app.js
@@ -3,22 +3,113 @@ var fs = require('fs');
 var https = require('https');
 var app = express();
 
+var PAGE_ACCESS_TOKEN = 'EAAGHIKQ5uIkBAKSzupCbKeRkZBWpSQq07ZAiZA04Ke6GLNBISnsLkO3k3U6BNOoHPpsqtQ6YvtGtuEjJdX6f7DgFJ3wTjAzkjxGivpHFiuOL6vOjhjLHsIV6sVZB1I9i4pS0eWWb03oRJbWQcRZCXyheNmY0Tu1XHLwc1dP35kgZDZD'
+
 app.get('/', function (req, res) {
   console.log("Received request");
   res.send('Hello World!');
 });
 
-app.get('/webhook', function(req, res) {
-  console.log("Received webhook");
-  if (req.query['hub.mode'] === 'subscribe' &&
-      req.query['hub.verify_token'] === 'freelancer_bot_hackathon') {
-    console.log("Validated webhook");
-    res.status(200).send(req.query['hub.challenge']);
-  } else {
-    console.error("Failed validation. Make sure the validation tokens match.");
-    res.sendStatus(403);
+app.post('/webhook', function (req, res) {
+  var data = req.body;
+
+  // Make sure this is a page subscription
+  if (data.object === 'page') {
+
+    // Iterate over each entry - there may be multiple if batched
+    data.entry.forEach(function(entry) {
+      var pageID = entry.id;
+      var timeOfEvent = entry.time;
+
+      // Iterate over each messaging event
+      entry.messaging.forEach(function(event) {
+        if (event.message) {
+          receivedMessage(event);
+        } else {
+          console.log("Webhook received unknown event: ", event);
+        }
+      });
+    });
+
+    // Assume all went well.
+    //
+    // You must send back a 200, within 20 seconds, to let us know
+    // you've successfully received the callback. Otherwise, the request
+    // will time out and we will keep trying to resend.
+    res.sendStatus(200);
   }
 });
+
+function receivedMessage(event) {
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var timeOfMessage = event.timestamp;
+  var message = event.message;
+
+  console.log("Received message for user %d and page %d at %d with message:",
+   senderID, recipientID, timeOfMessage);
+  console.log(JSON.stringify(message));
+
+  var messageId = message.mid;
+
+  var messageText = message.text;
+  var messageAttachments = message.attachments;
+
+  if (messageText) {
+
+   // If we receive a text message, check to see if it matches a keyword
+   // and send back the example. Otherwise, just echo the text we received.
+   switch (messageText) {
+     case 'generic':
+       sendGenericMessage(senderID);
+       break;
+
+     default:
+       sendTextMessage(senderID, messageText);
+   }
+  } else if (messageAttachments) {
+    sendTextMessage(senderID, "Message with attachment received");
+  }
+}
+
+function sendGenericMessage(recipientId, messageText) {
+  // To be expanded in later sections
+}
+
+function sendTextMessage(recipientId, messageText) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      text: messageText
+    }
+  };
+
+  callSendAPI(messageData);
+}
+
+function callSendAPI(messageData) {
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: PAGE_ACCESS_TOKEN },
+    method: 'POST',
+    json: messageData
+
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var recipientId = body.recipient_id;
+      var messageId = body.message_id;
+
+      console.log("Successfully sent generic message with id %s to recipient %s",
+        messageId, recipientId);
+    } else {
+      console.error("Unable to send message.");
+      console.error(response);
+      console.error(error);
+    }
+  });
+}
 
 https.createServer({
   key: fs.readFileSync('/etc/letsencrypt/live/freelancerbot.flndev.com/privkey.pem'),
