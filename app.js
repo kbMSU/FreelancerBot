7bc2012343022266f62f2487d71ec739bd7b3004
whanata <whanata1@gmail.com>
2017-02-17 12:12:26 +1100

    Allow View of 4 Latest Contest as a List

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var config = require('config');
 var crypto = require('crypto');
 var request = require('request');
 
+var Contest = require('./contest.js');
+
 var app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
@@ -77,18 +79,22 @@ function receivedMessage(event) {
   var messageText = message.text;
   var messageAttachments = message.attachments;
 
-  if (messageText) {
+  var contest = new Contest();
 
+  if (messageText) {
+    console.log(messageText);
    // If we receive a text message, check to see if it matches a keyword
    // and send back the example. Otherwise, just echo the text we received.
-   switch (messageText) {
-     case 'generic':
-       sendGenericMessage(senderID);
-       break;
-
-     default:
-       sendTextMessage(senderID, messageText);
-   }
+    if (contest.sayContest(messageText)) {
+      console.log('contest');
+      contest.getContests()
+        .then(function (response) {
+          var contestList = response.data;
+          sendListMessage(senderID, contest.getElementList(contestList));
+        });
+    } else {
+      sendTextMessage(senderID, messageText);
+    }
   } else if (messageAttachments) {
     sendTextMessage(senderID, "Message with attachment received");
   }
@@ -105,6 +111,33 @@ function sendTextMessage(recipientId, messageText) {
     },
     message: {
       text: messageText
+    }
+  };
+
+  callSendAPI(messageData);
+}
+
+function sendListMessage(recipientId, elementList) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      attachment: {
+        type: 'template',
+        payload: {
+          template_type: 'list',
+          top_element_style: 'compact',
+          elements: elementList,
+          buttons: [
+            {
+              title: 'View More',
+              type: 'postback',
+              payload: 'payload'                        
+            }
+          ]  
+        }
+      }
     }
   };
 
---
+++ contest.js
@@ -0,0 +1,48 @@
+var axios = require('axios');
+
+// Constructor
+function Contest() {
+  // always initialize all instance properties
+  this.textMessage = '';
+}
+// class methods
+Contest.prototype.sayContest = function(textMessage) {
+  var re = new RegExp('^contest$');
+
+  this.textMessage = textMessage;
+  if (re.test(textMessage)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+Contest.prototype.getContests = function() {
+  return axios.get('https://www.freelancer.com/api/contests/0.1/contests/?statuses[]=active&limit=4', {
+    transformResponse: [function (data) {
+      var dataJson = JSON.parse(data);
+      return dataJson.result.contests;
+    }],
+  });
+}
+
+Contest.prototype.getElementList = function(contestList) {
+  var elementList = [];
+
+  for (var i = 0; i < contestList.length; i++) {
+    var contest = contestList[i];
+    var seo_url = 'https://www.freelancer.com/' + contest.seo_url;
+
+    var element = {
+      title: contest.title,
+      subtitle: contest.description,
+      item_url: seo_url
+    }
+    elementList.push(element);
+  }
+
+  return elementList;
+}
+
+// export the class
+module.exports = Contest;
